4bb8b154f306fc3453c7628a8125f8425c7beaf7
Raffaele <izziaraffaele@gmail.com>
2016-05-14 11:08:45 +0200

    Implement cache

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace IRWeb\Orchestrate;
 
-use andrefelipe\Orchestrate\Application;
+use Cache;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Storage\LaravelCacheStorage;
+use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
+
+use andrefelipe\Orchestrate;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
@@ -29,17 +34,68 @@ class ServiceProvider extends BaseServiceProvider
     public function register()
     {
         $this->app->singleton('andrefelipe\Orchestrate\Application', function ($app) {
-            return new Application(
-              config('orchestrate.key'),
-              config('orchestrate.host'),
-              config('orchestrate.version')
-            );
+            $httpClient = $this->createHttpClient();
+
+            $application = new Orchestrate\Application();
+            $application->setHttpClient($httpClient);
+
+            if($cacheDriver = config('orchestrate.cache_driver')) {
+
+            }
+            return $application;
         });
-        $this->app->alias('orchestrate', 'andrefelipe\Orchestrate\Application');
+
+        $this->app->singleton('andrefelipe\Orchestrate\Client', function ($app) {
+            $httpClient = $this->createHttpClient();
+
+            $application = new Orchestrate\Client();
+            $application->setHttpClient($httpClient);
+
+            return $application;
+        });
     }
 
     public function provides()
     {
-        return ['andrefelipe\Orchestrate\Application'];
+        return [
+            'andrefelipe\Orchestrate\Client',
+            'andrefelipe\Orchestrate\Application',
+        ];
+    }
+
+    private function createHttpClient()
+    {
+        // Use andrefelipe helper function to get the base config array
+        $config = Orchestrate\default_http_config(
+            config('orchestrate.key'),
+            config('orchestrate.host'),
+            config('orchestrate.version')
+        );
+
+        if( $cacheDriver = config('orchestrate.cache_driver') ) 
+        {
+            $config['stack'] = $this->createCacheStack($cacheDriver);
+        }
+
+        return new \GuzzleHttp\Client($config);
+    }
+
+    private function createCacheStack($driver) 
+    {
+      // Create default HandlerStack
+      $stack = \GuzzleHttp\HandlerStack::create();
+
+      $stack->push(
+          new CacheMiddleware(
+              new GreedyCacheStrategy(
+                  new LaravelCacheStorage(
+                      Cache::store($driver)
+                  )
+              , 24 * 60 * 60) // 24 hours
+          ),
+          'cache'
+      );
+
+      return $stack;
     }
 }
--- src/config/orchestrate.php
+++ src/config/orchestrate.php
@@ -2,6 +2,8 @@
 
 return [
   'key' => env('ORCHESTRATE_API_KEY', 'your_api_key'),
-  'host' => 'https://api.aws-eu-west-1.orchestrate.io/',
-  'version' => 'v0'
-]
+  'host' => 'https://api.orchestrate.io/',
+  'version' => 'v0',
+
+  'cache_driver' => env('ORCHESTRATE_CACHE_DRIVER','array')
+];
